3e241a0016077219ce8a80ba5be4fc09913d5cbc
dnredson <dnredson@gmail.com>
2026-09-17 15:27:55 -0300

    Stop exporting embedded Irrigap defaults

--- src/smarter_adapter/parsers/__init__.py
+++ src/smarter_adapter/parsers/__init__.py
@@ -1,13 +1,8 @@
 """Parser plugins shipped with Smarter Adapter 2.0."""
 
-from .chirpstack_irrigap import (
-    DEFAULT_IRRIGAP_NODES,
-    IrrigapChirpStackParser,
-    IrrigapNode,
-)
+from .chirpstack_irrigap import IrrigapChirpStackParser, IrrigapNode
 
 __all__ = [
-    "DEFAULT_IRRIGAP_NODES",
     "IrrigapChirpStackParser",
     "IrrigapNode",
 ]
