7fc92997ec4dc8ca6501dc039142c3050811987e
dnredson <dnredson@gmail.com>
2026-09-17 16:51:59 -0300

    Adapt lifecycle fake to typed profile resolution

--- tests/test_v2_catalog_lifecycle.py
+++ tests/test_v2_catalog_lifecycle.py
@@ -49,7 +49,7 @@ class _FailingControl:
     def ensure_base(self, **kwargs):
         return self.base
 
-    def ensure_device_type(self, workspace_id):
+    def ensure_device_type(self, workspace_id, **kwargs):
         return self.device_type
 
     def ensure_device(self, workspace_id, channel_id, external_id, **kwargs):
